848ed5edfc48fcb3280f83351eff131504ce21e9
Ekaterina Filipenya <katerina.filipenua@gmail.com>
2020-10-23 12:29:51 +0300

    Add parent class for test-classes to project

---
+++ src/test/java/test/CommonConditions.java
@@ -0,0 +1,23 @@
+package test;
+
+import driver.DriverSingleton;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import utils.TestListener;
+
+@Listeners({TestListener.class})
+public class CommonConditions {
+    protected WebDriver driver;
+
+    @BeforeMethod(alwaysRun = true)
+    public void browserSetup() {
+        driver = DriverSingleton.getDriver();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void browserTearDown() {
+        DriverSingleton.closeDriver();
+    }
+}
